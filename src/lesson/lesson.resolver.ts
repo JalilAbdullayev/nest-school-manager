56209d1fbcda34f18d850cbdad7faa8bf3e542e5
Jalil Abdullayev <abdullacelil00@gmail.com>
2025-01-26 15:53:19 +0400

    Add student retrieval to lessons
    - Expose student service and implement student field resolution on the lesson type, which retrieves students related to a specific lesson.

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,14 +1,24 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { Lesson } from './lesson.entity';
 import { CreateLessonInput } from './lesson.input';
 import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
-import { DeepPartial } from 'typeorm';
+import { StudentService } from '../student/student.service';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
-  constructor(private readonly lessonService: LessonService) {}
+  constructor(
+    private readonly lessonService: LessonService,
+    private readonly studentService: StudentService,
+  ) {}
 
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string): Promise<Lesson | null> {
@@ -34,5 +44,10 @@ export class LessonResolver {
   ): Promise<Lesson> {
     const { lessonId, studentIds } = assignStudentToLessonInput;
     return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
   }
 }
